31ecef6ba3f11c336074d5802b0d94245cab6009
Floris Kruger <florisjkruger4@gmail.com>
2023-01-03 22:47:55 -0600

    added whitenoise

--- Online_Portfolio/settings.py
+++ Online_Portfolio/settings.py
@@ -21,6 +21,7 @@ ALLOWED_HOSTS = ['www.floriskruger.com', 'onlineportfolio-production.up.railway.
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -33,6 +34,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,16 +111,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
-
 STATIC_URL = 'staticfiles/'
+MEDIA_URL = 'media/'
 
-STATIC_ROOT = "/www/floriskruger.com/staticfiles/"
+if DEBUG:
+  STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
-MEDIA_ROOT = ''
-MEDIA_URL = ''
+else:
+  STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
+    
     path('', views.homePage, name="home"),
 ]
 
